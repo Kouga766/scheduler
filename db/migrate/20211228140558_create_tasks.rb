7bcce942dfedbba76484cd3b5d8d31763d5229cd
Kouga766 <houkenkokushi@gmail.com>
2021-12-28 14:13:36 +0000

    Initial Commit

---
+++ db/migrate/20211228140558_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      
+      t.string  :title,        :null => false
+      t.string  :detail
+      t.integer  :priority,        :null => false
+      
+      
+      
+      t.timestamps
+    end
+  end
+end
