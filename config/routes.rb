2a06aa9158df158c5a02d7280209df416cbf4a48
Kouga766 <houkenkokushi@gmail.com>
2021-12-30 11:10:42 +0000

    home削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+  unauthenticated :user do
+    root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 end
